650895d6727aaa7d584ec6d0e3037234cd0408bf
dalvidivesh1@gmail.com <dalvidivesh1@gmail.com>
2023-04-22 13:12:55 +0530

    made root canvas

--- task_manager.py
+++ task_manager.py
@@ -8,17 +8,41 @@ mydb = mysql.connector.connect(
     database = "tasks"
 )
 mycursor = mydb.cursor()
+def add_task():
+    task_name = task_name_tb.get()
+    task_description = task_description_tb.get()
+    query = "INSERT INTO task_list (task, description) VALUES (%s, %s)"
+    value = (task_name, task_description)
+    mycursor.execute(query,value)
+    mydb.commit()
+    task_list.insert(END,task_name)
+
+def update_task():
+    task_name = task_name_tb.get()
+    task_description = task_description_tb.get()
+    query = "UPDATE task_list SET description = %s where task = %s"
+    value = (task_description,task_name)
+    mycursor.execute(query,value)
+    mydb.commit()
+    task_list.delete(ACTIVE)
+    task_list.insert(ACTIVE,task_name)
+
+def delete_task():
+    task_name = task_name_tb.get()
+    query = "DELETE FROM task_list WHERE task = %s"
+    value = (task_name,)
+    mycursor.execute(query,value)
+    mydb.commit()
+    task_list.delete(ACTIVE)
+
+
+
 
 root = Tk()
-#Enter Task In database
-# sql = "INSERT INTO task_list (task, description) VALUES (%s, %s)"
-# val = ("Task 1", "This is task 1")
-# mycursor.execute(sql, val)
-# mydb.commit()
+root.title("Task Manager")
+root.geometry("800x700")
+task_label = Label(root,text="Task: " ,font=("Helvetica", 14))
+task_label.configure(fg="blue", bg="yellow")
+task_label.place(x =10,y=10)
 
-# to retrieve data from database
-# mycursor.execute("SELECT * FROM task_list")
-# myresult = mycursor.fetchall()
-# for task in myresult:
-#     print(task)
-
+root.mainloop()
